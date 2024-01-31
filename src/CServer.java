b32a6eb2138b17a208e73030a8dabdfa5500e711
matimatu <s4544717@studenti.unige.it>
2024-01-31 14:31:07 +0100

    server

---
+++ src/CServer.java
@@ -0,0 +1,251 @@
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.Semaphore;
+
+class CServerThread extends Thread {
+    Socket client1;
+    Socket client2;
+    String messageReceivedFromClient1;
+    BufferedReader inFromClient1;
+    PrintWriter outToClient1;
+    PrintWriter outToClient2;
+    String playerNickname;
+    CBattleShip battleShip;
+    CBattleShip.CPlayer player1;
+    CBattleShip.CPlayer player2;
+    Semaphore semaphore;
+    boolean myTurn;
+
+    boolean done;
+
+
+    public CServerThread(Socket socket1, Socket socket2,
+                         CBattleShip battleShip, CBattleShip.CPlayer player1,CBattleShip.CPlayer player2, Semaphore semaphore,boolean myTurn) {
+        this.client1 = socket1;
+        this.client2 = socket2;
+        this.battleShip = battleShip;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.semaphore = semaphore;
+        this.myTurn = myTurn;
+    }
+
+    public void run(){
+        try{
+            communicate();
+        }catch (Exception e){
+            e.printStackTrace(System.out);  }
+    }
+
+    public void communicate(){
+        try
+        {
+            inFromClient1 = new BufferedReader(new InputStreamReader(client1.getInputStream()));
+            outToClient2 = new PrintWriter(client2.getOutputStream(),true);
+            outToClient1 = new PrintWriter(client1.getOutputStream(),true);
+            //setting player name
+            handleNickname();
+            startGame();
+            while(!done) //infinite loop to continue communication
+            {
+                messageReceivedFromClient1 = inFromClient1.readLine();
+                if (messageReceivedFromClient1 == null || messageReceivedFromClient1.startsWith("/quit"))
+                {
+                    outToClient1.println(" (=>leaving the chat...)" + "\n");
+                    System.out.println(playerNickname + " requested to leave the chat  :" + messageReceivedFromClient1);
+                    break;
+                }
+                else
+                {
+                    outToClient2.println(playerNickname + ": " + messageReceivedFromClient1 + "\n");
+                    System.out.println("Echo on server :" + messageReceivedFromClient1);
+                }
+            }
+            outToClient1.close();
+            inFromClient1.close();
+            client1.close();
+            System.out.println(client1 + " closed ");
+            outToClient2.println(" (=>" + playerNickname + " leaved the chat)" + "\n");
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Server communicate error!");
+            System.exit(1);
+        }
+    }
+
+    private void handleNickname()
+    {
+        try
+        {
+            System.out.println("sending info to client for login...");
+            outToClient1.println("Please enter a nickname before playing");
+            playerNickname = inFromClient1.readLine();
+            System.out.println("user " + playerNickname + " logged in!");
+            outToClient1.println("Welcome to the battleship game " + playerNickname + "!");
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Server handle nickname error!");
+            System.exit(1);
+        }
+    }
+    private void startGame()
+    {
+        outToClient1.println("Here are the rules of battleship: ");
+        battleShip.generateRules();
+        outToClient1.println("Now we can start!");
+        try
+        {
+            shipPositioning();
+            semaphore.release();
+            if(semaphore.availablePermits() == 1)
+                outToClient1.println("Waiting the other player to finish the ship positioning...");
+            if(semaphore.availablePermits() == 2)
+            {
+                outToClient1.println("OK now we are ready to... battle!!");
+                startBattle();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println("error on waiting other threads!");
+            System.exit(1);
+
+        }
+    }
+
+    private void shipPositioning(){
+        try{
+            outToClient1.println("Let's insert the first ship, the little one!" +
+                    " Just type the coordinates starting from the character");
+            String input1 = inFromClient1.readLine();
+            //TODO OOOOOOOOOOOOOOOOO
+            while (!battleShip.insertShip(player1, CBattleShip.ShipType.little, input1))
+                outToClient1.println("Wrong coordinates,try again!");
+            outToClient1.println("Good! Now insert the second ship, the medium one!" +
+                    " Just type the coordinates starting from the character and separate the 2 coordinates with a comma");
+            String input2 = inFromClient1.readLine();
+            while(!battleShip.insertShip(player1, CBattleShip.ShipType.medium, input2))
+                outToClient1.println("Wrong coordinates,try again!");
+            outToClient1.println("Good! Now insert the third ship, the large one!" +
+                    " Just type the coordinates starting from the character and separate the 3 coordinates with a comma");
+            String input3 = inFromClient1.readLine();
+            while(!battleShip.insertShip(player1, CBattleShip.ShipType.large, input3))
+                outToClient1.println("Wrong coordinates,try again!");
+        }
+      catch (IOException e){
+          System.out.println(e.getMessage());
+          System.out.println("ERROR on ship positioning!");
+          shutdown();
+      }
+    }
+
+    private void startBattle(){
+        try{
+            while (!myTurn)
+            {
+                //wait
+            }
+            if(myTurn)
+            {
+                outToClient1.println("It's your turn!");
+                outToClient1.println("Type the coordinates to fire!");
+                String coord = inFromClient1.readLine();
+                CBattleShip.ShotOutcome shotOutcome = battleShip.fire(player2,coord);
+                myTurn = false;
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("ERROR on reading coordinates of the shot from the player!");
+            shutdown();
+        }
+    }
+
+
+    public void shutdown()
+    {
+        try
+        {
+            inFromClient1.close();
+            outToClient1.close();
+            if(!client1.isClosed())
+            {
+                client1.close();
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("server thread ERROR on shutting down client connection");
+        }
+    }
+
+
+}
+
+public class CServer {
+
+    ServerSocket server;
+    boolean done;                        // true if the server has to shut down
+
+    CServerThread serverThread1 ;
+    CServerThread serverThread2 ;
+    public void start(){
+        try{
+            server = new ServerSocket(6789);
+            System.out.println("Server waiting for first player... ");
+            Socket socket1 = server.accept();
+            System.out.println("Server socket  " + socket1);
+            System.out.println("Server waiting for second player... ");
+            Socket socket2 = server.accept();
+            System.out.println("Server socket  " + socket2);
+            System.out.println("Cannot handle other connections");
+            CBattleShip battleShip = new CBattleShip();
+            CBattleShip.CPlayer player1 = new CBattleShip.CPlayer();
+            CBattleShip.CPlayer player2 = new CBattleShip.CPlayer();
+            //creating 2 threads to handle 2 different clients
+            Semaphore semaphore = new Semaphore(0);
+            serverThread1 = new CServerThread(socket1,socket2,battleShip,player1,player2,semaphore,true);
+            serverThread1.start();
+            serverThread2 = new CServerThread(socket2,socket1,battleShip,player2,player1,semaphore,false);
+            serverThread2.start();
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Server acceptance error!");
+            shutdown();
+        }
+    }
+
+    public void shutdown(){
+        done = true;
+        try{
+            if(!server.isClosed())
+                server.close();
+            //shutdown of all the other connections
+            serverThread1.shutdown();
+            serverThread2.shutdown();
+
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Server shutdown error!");
+            System.exit(1);
+        }
+    }
+
+    public static void main (String[] args){
+        CServer chatServer = new CServer();
+        chatServer.start();
+    }
+}
